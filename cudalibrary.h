4bde7264b99af841f8915f3afbc90bb41c6e9450
Guraku-Black <112281765+Guraku-Black@users.noreply.github.com>
2022-09-19 21:43:07 -0400

    Add files via upload

--- cudalibrary.h
+++ cudalibrary.h
@@ -43,10 +43,19 @@ extern "C"
 		cudaNeuralUnit* arrayData;
 	} cudaNeuralArray;
 
-	DLLEXPORT long cudaNeuralArrayCreate(cudaNeuralArray* parent, unsigned long length);
+	DLLEXPORT long cudaNeuralArrayCreate(cudaNeuralArray* parent, unsigned long width, unsigned long height, unsigned long depth);
 	DLLEXPORT long cudaNeuralArraySetData(cudaNeuralArray* parent, cudaNeuralUnit* buffer, unsigned long buffersize);
 	DLLEXPORT long cudaNeuralArrayGetData(cudaNeuralArray* parent, cudaNeuralUnit* buffer, unsigned long buffersize);
 	DLLEXPORT long cudaNeuralArrayCopy(cudaNeuralArray* parent, cudaNeuralArray* source);
+	DLLEXPORT long cudaNeuralArraySetImageRGB(cudaNeuralArray* parent, unsigned long imagewidth, unsigned long imageheight, unsigned long* imagedata);
+	DLLEXPORT long cudaNeuralArrayGetImageRGB(cudaNeuralArray* parent, unsigned long imagewidth, unsigned long imageheight, unsigned long* imagedata);
+	DLLEXPORT long cudaNeuralArrayFillZero(cudaNeuralArray* result);
+	DLLEXPORT long cudaNeuralArrayFillOnes(cudaNeuralArray* result);
+	DLLEXPORT long cudaNeuralArrayFillRandom(cudaNeuralArray* result);
+	DLLEXPORT long cudaNeuralArrayAdd(cudaNeuralArray* result, cudaNeuralArray* value1, cudaNeuralArray* value2);
+	DLLEXPORT long cudaNeuralArraySubtract(cudaNeuralArray* result, cudaNeuralArray* value1, cudaNeuralArray* value2);
+	DLLEXPORT long cudaNeuralArrayMultiply(cudaNeuralArray* result, cudaNeuralArray* value1, cudaNeuralArray* value2);
+	DLLEXPORT long cudaNeuralArrayDivide(cudaNeuralArray* result, cudaNeuralArray* value1, cudaNeuralArray* value2);
 	DLLEXPORT long cudaNeuralArrayReverse(cudaNeuralArray* parent, cudaNeuralArray* source);
 	DLLEXPORT long cudaNeuralArrayDestroy(cudaNeuralArray* parent);
 
